41194a8c5359ef5df5eb23aa531e98d26425c664
Preethi R <preethiravikumar07@gmail.com>
2022-07-14 18:10:12 +0530

    First commit

---
+++ StudentPortfolio/Models/StudentDBcontext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortfolio.Models
+{
+    public class StudentDBcontext:DbContext
+    {
+        public StudentDBcontext(DbContextOptions<StudentDBcontext> options):base(options)
+        {
+
+        }
+        public DbSet<StudentModel> Students { get; set; }
+    }
+}
---
+++ StudentPortfolio/Models/StudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortfolio.Models
+{
+    public class StudentModel
+    {
+        [Required(ErrorMessage ="Please enter student Id")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Location { get; set; }
+        public int Rank { get; set; }
+    }
+}
